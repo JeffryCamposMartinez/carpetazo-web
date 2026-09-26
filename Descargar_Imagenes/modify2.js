99c099a7e0af523117e0ba055f32a1988dfe2afc
Jeffry Marcel Campos Martinez <jeffry.campos.martinez@gmail.com>
2026-09-26 17:09:17 -0300

    Delete Descargar_Imagenes directory

--- Descargar_Imagenes/modify2.js
+++
@@ -1,7 +0,0 @@
-﻿const fs = require("fs");
-let code = fs.readFileSync("server.js", "utf8");
-
-if (!code.includes("/api/failed")) {
-  code = code.replace("app.listen(PORT", "app.get('/api/failed', (req, res) => {\n  res.json(failedDownloads);\n});\n\napp.listen(PORT");
-  fs.writeFileSync("server.js", code);
-}
